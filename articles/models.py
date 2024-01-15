ed5646e97520100942cf2c5931773e2955b7361d
bigetti <upgrade81@mail.ru>
2024-01-15 16:41:35 +0300

    init commit

---
+++ articles/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название тега')
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope', verbose_name='Теги')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False, verbose_name='Остновной тег')
+
+    class Meta:
+        unique_together = ('article', 'is_main')
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name}"
